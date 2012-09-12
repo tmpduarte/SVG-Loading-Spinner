ebb1e3e2217141e0683e0067d3d7b5c08868aedd
Tiago Duarte <tiago.duarte@centralway.com>
2012-09-12 22:13:15 +0200

    changed the order of the divs in the spinner, now the spinner stays on top

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ function unleashSpinner() {
     data.centerRadius = 35;
     data.sectorLength = 50;
     data.sectorsCount = 10;
-    data.sectorWidth = 15;
-    data.color = 'red';
+    data.sectorWidth = 20;
+    data.color = 'white';
     data.fullScreenOpacity = 0.8;
 
     var spinner = new Spinner(data);
 
     spinner.create();
-    setTimeout(function(){spinner.destroy();}, 3000);
+    setTimeout(function(){spinner.destroy();}, 6000);
 
     return false;
 }
